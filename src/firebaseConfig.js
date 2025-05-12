04cd82e1e8fcf16bc321c722010af9be96dad61c
Siamax <ibrahimahtsham2002@gmail.com>
2025-05-12 12:15:02 +0500

    pages deployed

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,18 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCHbX7K9XA-uOtB_04Fjsn7H989gsnVT8I",
-  authDomain: "meezan-funds-to-graph.firebaseapp.com",
-  projectId: "meezan-funds-to-graph",
-  storageBucket: "meezan-funds-to-graph.appspot.com",
-  messagingSenderId: "572547169604",
-  appId: "1:572547169604:web:3616ab03929e51ca7feb11",
-  measurementId: "G-9QQT19QGPP",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY, // e.g., "AIzaSy..."
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN, // e.g., "your-project.firebaseapp.com"
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID, // e.g., "your-project-id"
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET, // e.g., "your-project.appspot.com"
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID, // e.g., "1234567890"
+  appId: process.env.REACT_APP_FIREBASE_APP_ID, // e.g., "1:1234567890:web:abcdefg"
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID, // optional
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
